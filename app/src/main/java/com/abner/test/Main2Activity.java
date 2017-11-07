82f6c47e73cf651ab7c7a9a4121c27588af7915a
songheng <729818876@qq.com>
2017-11-07 11:31:14 +0800

    2.0.0

---
+++ app/src/main/java/com/abner/test/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.abner.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void getOnClick(){
+        for(int i = 0;i<10;i++){
+            System.out.print("fjhefhojfjfpejfopjjjlj");
+            System.out.print("fjhefhojfjfpejfopj腹黑oh覅额哈佛和of饿哦if哦jjlj");
+        }
+    }
+}
