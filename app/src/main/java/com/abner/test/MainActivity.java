a366e8cf21add2b67c00d458ef60dda59ee8b40c
songheng <729818876@qq.com>
2017-11-07 11:28:26 +0800

    1.0.0

--- app/src/main/java/com/abner/test/MainActivity.java
+++ app/src/main/java/com/abner/test/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void getOnClick(){
         for(int i = 0;i<10;i++){
+            System.out.print("fjhefhojfjfpejfopded3t4ty4g3fwf23fjjjlj");
             System.out.print("fjhefhojfjfpejfopjjjlj");
         }
     }
